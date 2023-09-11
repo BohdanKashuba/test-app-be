8a61d19df7c9eaa692e610cad14bc734a152f13d
Bogdan <bohdan.kashuba.work@gmail.com>
2023-09-11 17:04:39 +0300

    Fix. Resolve path to tmp file

--- src/shared/file/file.service.ts
+++ src/shared/file/file.service.ts
@@ -1,21 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { v4 as uuid } from 'uuid';
 import path from 'path';
 import fs from 'fs';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class FileService {
+  constructor(
+    @Inject(ConfigService) private readonly configService: ConfigService,
+  ) {}
+
   upload(file: Express.Multer.File) {
     const ext = path.extname(file.originalname);
     const filename = uuid() + ext;
     const fileUrl = path.join('images', filename);
 
-    fs.writeFileSync(path.join(process.cwd(), 'tmp', fileUrl), file.buffer);
+    const nodeEnv = this.configService.get('NODE_ENV');
+
+    if (nodeEnv === 'dev') {
+      fs.writeFileSync(path.join(process.cwd(), 'tmp', fileUrl), file.buffer);
+    } else {
+      fs.writeFileSync(path.join('/tmp', fileUrl), file.buffer);
+    }
 
     return fileUrl;
   }
 
   destroy(fileUrl: string) {
-    fs.unlinkSync(path.join(process.cwd(), 'tmp', fileUrl));
+    const nodeEnv = this.configService.get('NODE_ENV');
+
+    if (nodeEnv === 'dev') {
+      fs.unlinkSync(path.join(process.cwd(), 'tmp', fileUrl));
+    } else {
+      fs.unlinkSync(path.join('/tmp', fileUrl));
+    }
   }
 }
